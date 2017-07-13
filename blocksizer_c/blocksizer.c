96e0665a8203f11a070d2f7b8cd2107788e8e1e3
ren <code@apotheon.net>
2017-07-13 22:56:32 +0000

    rearrange some stuff in blocksizer.c

--- blocksizer_c/blocksizer.c
+++ blocksizer_c/blocksizer.c
@@ -23,55 +23,17 @@
  * squashed).  All blame and dire consequence for actually writing it resides
  * solely with the 2017 Copyright of Chad Perrin.  So mote it be.
  *
- * This work is released under the terms of the COIL.
- *
- * # Copyfree Open Innovation License
- *
- * This is version 0.5 of the Copyfree Open Innovation License.
- *
- * ## Terms and Conditions
- *
- * Redistributions, modified or unmodified, in whole or in part, must retain
- * applicable copyright or other legal privilege notices, these conditions, and
- * the following license terms and disclaimer.  Subject to these conditions,
- * the holder(s) of copyright or other legal privileges, author(s) or
- * assembler(s), and contributors of this work hereby grant to any person who
- * obtains a copy of this work in any form:
- *
- * 1. Permission to reproduce, modify, distribute, publish, sell, sublicense,
- * use, and/or otherwise deal in the licensed material without restriction.
- *
- * 2. A perpetual, worldwide, non-exclusive, royalty-free, irrevocable patent
- * license to reproduce, modify, distribute, publish, sell, use, and/or
- * otherwise deal in the licensed material without restriction, for any and all
- * patents:
- *
- *    a. Held by each such holder of copyright or other legal privilege, author
- *    or assembler, or contributor, necessarily infringed by the contributions
- *    alone or by combination with the work, of that privilege holder, author
- *    or assembler, or contributor.
- *
- *    b. Necessarily infringed by the work at the time that holder of copyright
- *    or other privilege, author or assembler, or contributor made any
- *    contribution to the work.
- *
- * NO WARRANTY OF ANY KIND IS IMPLIED BY, OR SHOULD BE INFERRED FROM, THIS
- * LICENSE OR THE ACT OF DISTRIBUTION UNDER THE TERMS OF THIS LICENSE,
- * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR
- * A PARTICULAR PURPOSE, AND NONINFRINGEMENT.  IN NO EVENT SHALL THE AUTHORS,
- * ASSEMBLERS, OR HOLDERS OF COPYRIGHT OR OTHER LEGAL PRIVILEGE BE LIABLE FOR
- * ANY CLAIM, DAMAGES, OR OTHER LIABILITY, WHETHER IN ACTION OF CONTRACT, TORT,
- * OR OTHERWISE ARISING FROM, OUT OF, OR IN CONNECTION WITH THE WORK OR THE USE
- * OF OR OTHER DEALINGS IN THE WORK.
+ * This work is released under the terms of the COIL.  See end of file for
+ * explicit license terms.
  */
 
 int is_numeric(char *string_argument);
-int wrap_whitespace(int length, char *string, int start);
 int match_count(char *string_argument);
 int match_help(char *string_argument);
 int match_string(char *s, char *m);
 int power(int base, unsigned int exponent);
 int print_wrap(int wrap, char *string, int position);
+int wrap_whitespace(int length, char *string, int start);
 
 unsigned long blocksize(unsigned long filesize);
 unsigned long get_filesize(char *filesize_string);
@@ -318,3 +280,44 @@ unsigned long get_size_from_file(const char *filename) {
 
     return bytesize;
 }
+
+/*
+ * # Copyfree Open Innovation License
+ *
+ * This is version 0.5 of the Copyfree Open Innovation License.
+ *
+ * ## Terms and Conditions
+ *
+ * Redistributions, modified or unmodified, in whole or in part, must retain
+ * applicable copyright or other legal privilege notices, these conditions, and
+ * the following license terms and disclaimer.  Subject to these conditions,
+ * the holder(s) of copyright or other legal privileges, author(s) or
+ * assembler(s), and contributors of this work hereby grant to any person who
+ * obtains a copy of this work in any form:
+ *
+ * 1. Permission to reproduce, modify, distribute, publish, sell, sublicense,
+ * use, and/or otherwise deal in the licensed material without restriction.
+ *
+ * 2. A perpetual, worldwide, non-exclusive, royalty-free, irrevocable patent
+ * license to reproduce, modify, distribute, publish, sell, use, and/or
+ * otherwise deal in the licensed material without restriction, for any and all
+ * patents:
+ *
+ *    a. Held by each such holder of copyright or other legal privilege, author
+ *    or assembler, or contributor, necessarily infringed by the contributions
+ *    alone or by combination with the work, of that privilege holder, author
+ *    or assembler, or contributor.
+ *
+ *    b. Necessarily infringed by the work at the time that holder of copyright
+ *    or other privilege, author or assembler, or contributor made any
+ *    contribution to the work.
+ *
+ * NO WARRANTY OF ANY KIND IS IMPLIED BY, OR SHOULD BE INFERRED FROM, THIS
+ * LICENSE OR THE ACT OF DISTRIBUTION UNDER THE TERMS OF THIS LICENSE,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR
+ * A PARTICULAR PURPOSE, AND NONINFRINGEMENT.  IN NO EVENT SHALL THE AUTHORS,
+ * ASSEMBLERS, OR HOLDERS OF COPYRIGHT OR OTHER LEGAL PRIVILEGE BE LIABLE FOR
+ * ANY CLAIM, DAMAGES, OR OTHER LIABILITY, WHETHER IN ACTION OF CONTRACT, TORT,
+ * OR OTHERWISE ARISING FROM, OUT OF, OR IN CONNECTION WITH THE WORK OR THE USE
+ * OF OR OTHER DEALINGS IN THE WORK.
+ */
